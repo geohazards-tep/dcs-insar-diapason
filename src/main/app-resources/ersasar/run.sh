858d92780b6a5f3495a90a48a7fb2358b9158527
pordoqui <patrick.ordoqui@altamira-information.com>
2015-08-31 10:12:47 +0200

    - process only the overlapping area between the two SAR images - fix the calls to the ciop-log function

--- src/main/app-resources/ersasar/run.sh
+++ src/main/app-resources/ersasar/run.sh
@@ -15,7 +15,7 @@ ERRMISSING=255
 function procCleanup()
 {
     if [ -n "${serverdir}"  ] && [ -d "$serverdir" ]; then
-	ciop-log "INFO : Cleaning up processing directory ${serverdir}"
+	ciop-log "INFO" "Cleaning up processing directory ${serverdir}"
 	rm -rf "${serverdir}"
     fi
     
@@ -25,7 +25,7 @@ function procCleanup()
 function trapFunction()
 {
     procCleanup
-    ciop-log "ERROR : Signal was trapped"
+    ciop-log "ERROR" "Signal was trapped"
     exit 
 }
 
@@ -61,13 +61,13 @@ function demDownload()
     
     #check for required programs 
     if [ -z "`type -p curl`" ] ; then
-	ciop-log "ERROR : System missing curl utility" return
-	${ERRMISSING} 
+	ciop-log "ERROR" "System missing curl utility" 
+	return ${ERRMISSING} 
     fi
 	
     if [ -z "`type -p gdalinfo`" ] ; then
-	ciop-log "ERROR : System missing gdalinfo utility" return
-	${ERRMISSING} 
+	ciop-log "ERROR"  "System missing gdalinfo utility" 
+	return ${ERRMISSING} 
     fi
 
 
@@ -87,7 +87,7 @@ function demDownload()
     
     url="http://www.altamira-information.com/demdownload?lat="${latitudes[0]}"&lat="${latitudes[1]}"&lon="${longitudes[0]}"&lon="${longitudes[1]}
     
-    ciop-log "INFO : Downloading DEM from ${url}"
+    ciop-log "INFO"  "Downloading DEM from ${url}"
     
     demtif=${procdir}/DAT/dem.tif
     
@@ -97,13 +97,13 @@ function demDownload()
 
     #check downloaded file
     if [ ! -e "${demtif}" ]; then
-	ciop-log "ERROR : Unable to download DEM data"
+	ciop-log "ERROR" "Unable to download DEM data"
 	return ${ERRGENERIC}
     fi
     
     #check it is a tiff
-    gdalinfo "${demtif}" || {
-	ciop-log "ERROR : No DEM data over selected area"
+    gdalinfo "${demtif}" >> "${procdir}"/log/demdownload.log 2<&1 || {
+	ciop-log "ERROR"  "No DEM data over selected area"
 	return ${ERRGENERIC}
     }
     
@@ -114,18 +114,49 @@ export DEM="${procdir}/DAT/dem.dat"
 
 
 if [ ! -e "${DEM}" ]; then
-    ciop-log "ERROR : Failed to convert downloaded DEM data"
+    ciop-log "ERROR"  "Failed to convert downloaded DEM data"
     msg=`cat "${procdir}/log/deminport.log"`
-    ciop-log "INFO : ${msg}"
+    ciop-log "INFO" "${msg}"
     
     return ${ERRGENERIC}
 fi
 
-ciop-log "INFO : DEM descriptor creation done "
+ciop-log "INFO"  "DEM descriptor creation done "
 
 return ${SUCCESS}
 
 }
+
+
+function check_local_sar_file
+{
+    
+    if [ $# -lt 1 ]; then
+	return ${ERRGENERIC}
+    fi
+
+    file=$1
+
+    fsize=$(stat -c "%s" "$file")
+    
+    if [ $fsize -gt 1048576 ]; then	
+	return ${SUCCESS}
+    fi
+    
+    #files with size < 1MB means download probably failed
+    #look for an html tag at the beginning of the file
+    tag=`dd if=${file} bs=10 count=1 | grep -o html`
+    
+    if [ -n "${tag}" ] && [ "${tag}" == "html" ]; then
+	ciop-log "INFO" "html tag detected in local file. You likely are not allowed to access the remote resource"
+	return ${ERRPERM}
+    fi 
+    
+    ciop-log "INFO" "local file size is < 1MB. Either the download failed , or access to the remote resource is denied"
+    
+    return ${ERRGENERIC}
+}
+
 
 #setup the Diapason environment
 export LANGUE=en
@@ -201,7 +232,7 @@ fi
 ciop-log "DEBUG" "Master $master and Slave $SLAVE"
 
 if [ -z "$MASTER" ] || [ -z "${SLAVE}" ] ; then
-    ciop-log "ERROR : Missing Input file . MASTER->$MASTER , SLAVE->$SLAVE "
+    ciop-log "ERROR"  "Missing Input file . MASTER->$MASTER , SLAVE->$SLAVE "
     exit ${ERRMISSING}
 fi 
 
@@ -210,11 +241,11 @@ fi
 #create processing directory
 unset serverdir
 export serverdir=`mktemp -d ${rootdir}/DIAPASON_XXXXXX` || {
-ciop-log "ERROR : Cannot create processing directory"
+ciop-log "ERROR " "Cannot create processing directory"
 exit ${ERRPERM}
 }
 
-ciop-log "INFO : processing in directory ${serverdir}"
+ciop-log "INFO"  "processing in directory ${serverdir}"
 
 #trap signals
 trap trapFunction SIGHUP SIGINT SIGTERM
@@ -222,20 +253,25 @@ trap trapFunction SIGHUP SIGINT SIGTERM
 
 #create directory tree
 mkdir -p ${serverdir}/{DAT/GEOSAR,RAW_C5B,SLC_CI2,ORB,TEMP,log,QC,GRID,DIF_INT,CD,GEO_CI2,VOR} || {
-ciop-log "ERROR : Cannot create processing directory structure"
+ciop-log "ERROR"  "Cannot create processing directory structure"
  exit ${ERRPERM}
 }
 
 
 
-#TO-DO check the input file type correctedness
 
 #stage-in the data
 localms=$( get_data ${MASTER} ${serverdir}/CD )
 #localms=`ciop-copy -o "${serverdir}/CD" "${MASTER}" `
 
 [  "$?" == "0"  -a -e "${localms}" ] || {
-    ciop-log "ERROR : Failed to download file ${MASTER}"
+    ciop-log "ERROR"  "Failed to download file ${MASTER}"
+    procCleanup
+    exit ${ERRSTGIN}
+}
+
+check_local_sar_file "${localms}" || {
+    ciop-log "ERROR"  "Unable to download file ${MASTER}"
     procCleanup
     exit ${ERRSTGIN}
 }
@@ -245,7 +281,13 @@ localsl=$( get_data ${SLAVE} ${serverdir}/CD )
 
 
 [  "$?" == "0"  -a -e "${localsl}" ] || {
-    ciop-log "ERROR : Failed to download file ${SLAVE}"
+    ciop-log "ERROR"  "Failed to download file ${SLAVE}"
+    procCleanup
+    exit ${ERRSTGIN}
+}
+
+check_local_sar_file "${localsl}" || {
+    ciop-log "ERROR" "Unable to download file ${SLAVE}"
     procCleanup
     exit ${ERRSTGIN}
 }
@@ -256,7 +298,7 @@ if [ ${flag_orbit_in} -gt 0 ]; then
     localmasterVOR=$( get_data ${MASTERVORURL} ${serverdir}/VOR )
     
     [  "$?" == "0"  -a -e "${localmasterVOR}" ] || {
-	ciop-log "ERROR : Failed to download file ${MASTERVORURL}"
+	ciop-log "ERROR"  "Failed to download file ${MASTERVORURL}"
 	procCleanup
 	exit ${ERRSTGIN}
     }
@@ -264,7 +306,7 @@ if [ ${flag_orbit_in} -gt 0 ]; then
     localslaveVOR=$( get_data ${SLAVEVORURL} ${serverdir}/VOR )
     
     [  "$?" == "0"  -a -e "${localslaveVOR}" ] || {
-	ciop-log "ERROR : Failed to download file ${SLAVEVORURL}"
+	ciop-log "ERROR"  "Failed to download file ${SLAVEVORURL}"
 	procCleanup
 	exit ${ERRSTGIN}
     }
@@ -278,7 +320,7 @@ find . -iname "*.tar" -exec tar -xvf '{}' \;
 cd -
 
 vorcontents=$(ls -l ${serverdir}/VOR)
-ciop-log "INFO : ${vorcontents}"
+
 #extract inputs
 
 #extract master
@@ -287,14 +329,14 @@ handle_tars.pl --in="${localms}"  --serverdir="${serverdir}" --exedir="${EXE_DIR
 orbitmaster=`grep -ih "ORBIT NUMBER" "${serverdir}/DAT/GEOSAR/"*.geosar | cut -b 40-1024 | sed 's@[[:space:]]@@g'`
 
 if [ -z "${orbitmaster}" ]; then
-	ciop-log "ERROR : Master image extraction failure"
+	ciop-log "ERROR"  "Master image extraction failure"
 	msg=`cat "${serverdir}"/log/extract_master.log`
-	ciop-log "ERROR : ${msg}"
+	ciop-log "ERROR"  "${msg}"
 	procCleanup
 	exit ${ERRGENERIC}
 fi
 
-ciop-log "INFO : Master orbit ${orbitmaster} extracted"
+ciop-log "INFO"  "Master orbit ${orbitmaster} extracted"
 
 #extract slave
 handle_tars.pl --in="${localsl}"  --serverdir="${serverdir}" --exedir="${EXE_DIR}" --tmpdir="${serverdir}/TEMP" > "${serverdir}"/log/extract_slave.log 2<&1
@@ -302,22 +344,22 @@ handle_tars.pl --in="${localsl}"  --serverdir="${serverdir}" --exedir="${EXE_DIR
 norbits=`ls "${serverdir}"/ORB/*.orb | wc -l`
 
 if [ $norbits -lt 2 ]; then
-    ciop-log "ERROR : Slave image extraction failure"
+    ciop-log "ERROR"  "Slave image extraction failure"
     msg=`cat "${serverdir}"/log/extract_slave.log`
-    ciop-log "ERROR : ${msg}"
+    ciop-log "ERROR"  "${msg}"
     procCleanup
     exit ${ERRGENERIC}
 fi
 
 orbitslave=`ls -tra "${serverdir}"/DAT/GEOSAR/*.geosar | tail -1 | xargs grep -ih "ORBIT NUMBER" | cut -b 40-1024 | sed 's@[[:space:]]@@g'`
 
-ciop-log "INFO : Slave orbit ${orbitslave} extracted"
+ciop-log "INFO"  "Slave orbit ${orbitslave} extracted"
 
 #a few checks
 npass=`grep -ih "SATELLITE PASS" "${serverdir}"/DAT/GEOSAR/*.geosar | cut -b 40-1024 | sort --unique | wc -l`
 
 if [ "$npass" != "1" ]; then
-    ciop-log "ERROR : images are of different satellite pass"
+    ciop-log "ERROR" "images are of different satellite pass"
     procCleanup
     exit ${ERRGENERIC}
 fi
@@ -326,7 +368,7 @@ fi
 for geosar in  `find "${serverdir}"/DAT/GEOSAR/ -iname "*.geosar" -print`; do
     sensor=`grep -i "SENSOR NAME" "${geosar}" | cut -b '40-1024' | sed 's@[[:space:]]@@g'`
     orbit=`grep -ih "ORBIT NUMBER" "${geosar}" | cut -b 40-1024 | sed 's@[[:space:]]@@g'`
-    ciop-log "INFO : Downloading precise orbit data for orbit ${orbit}"
+    ciop-log "INFO"  "Downloading precise orbit data for orbit ${orbit}"
     case "$sensor" in
 	ERS*) diaporb.pl --geosar="${geosar}" --type=delft  --outdir="${serverdir}/ORB" --exedir="${EXE_DIR}" >> "${serverdir}/log/precise_orbits.log" 2<&1 ;;
 	ENVISAT*) 
@@ -336,7 +378,7 @@ for geosar in  `find "${serverdir}"/DAT/GEOSAR/ -iname "*.geosar" -print`; do
 	    #try with remote orbits if first attempt fails
 	    if [ $storb -ne  0 ]; then
 		msg=`cat "${serverdir}/log/precise_orbits.log"`
-		ciop-log "INFO : ${msg}"
+		ciop-log "INFO" "${msg}"
 		diaporb.pl --geosar="${geosar}" --type=doris   --outdir="${serverdir}/ORB" --exedir="${EXE_DIR}" >> "${serverdir}/log/precise_orbits.log" 2<&1
 	    fi
 	    ;;
@@ -350,12 +392,19 @@ msroiopt=""
 slroiopt="";
 #get the master & slave  overlapping region 
 msovlp=`sarovlp.pl --geosarm="${serverdir}"/DAT/GEOSAR/${orbitmaster}.geosar --geosars="${serverdir}"/DAT/GEOSAR/${orbitslave}.geosar  --exedir="${EXE_DIR}" --nocols  | grep ^l1`
+ovlpstatus=$?
 
-[ -n "${msovlp}" ] && $msroiopt="--procroi ${msovlp}"
+if [ $ovlpstatus -eq 255 ]; then
+    ciop-log "ERROR " "Master and Slave image don't overlap "
+    procCleanup
+    exit ${ERRGENERIC}
+fi
+
+[ -n "${msovlp}" ] && msroiopt="--procroi ${msovlp}"
 
 slovlp=`sarovlp.pl --geosars="${serverdir}"/DAT/GEOSAR/${orbitmaster}.geosar --geosarm="${serverdir}"/DAT/GEOSAR/${orbitslave}.geosar  --exedir="${EXE_DIR}" --nocols | grep ^l1`
 
-[ -n "${slovlp}" ] && $slroiopt="--procroi ${slovlp}"
+[ -n "${slovlp}" ] && slroiopt="--procroi ${slovlp}"
 
 
 for geosar in `find "${serverdir}"/DAT/GEOSAR/ -iname "*.geosar" -print`; do
@@ -373,15 +422,15 @@ for geosar in `find "${serverdir}"/DAT/GEOSAR/ -iname "*.geosar" -print`; do
 		fi
 
 
-		ciop-log "INFO : Running L0 -> L1 processing for orbit ${orbitnum}"
-		ciop-log "INFO" "prisme.pl --geosar=$geosar --mltype=byt --dir=${serverdir}/SLC_CI2 --outdir=${serverdir}/SLC_CI2 --rate --exedir=${EXE_DIR} > ${serverdir}/log/prisme_${orbitnum}.log 2<&1"
+		ciop-log "INFO" "Running L0 -> L1 processing for orbit ${orbitnum}"
+		
 		ciop-log "INFO" "Area cropping : ${roiopt}"
 		prisme.pl --geosar="$geosar" --mltype=byt --tmpdir="${serverdir}/SLC_CI2" --outdir="${serverdir}/SLC_CI2" --rate --exedir="${EXE_DIR}" ${roiopt}  > ${serverdir}/log/prisme_${orbitnum}.log 2<&1
 		
 		[ "$?" == "0" ] || {
-		   ciop-log "ERROR : L0 -> L1 processing failed for orbit ${orbitnum}"
+		   ciop-log "ERROR"  "L0 -> L1 processing failed for orbit ${orbitnum}"
 		   msg=`cat ${serverdir}/log/prisme_${orbitnum}.log`
-		   ciop-log "INFO : $msg"
+		   ciop-log "INFO"  "$msg"
 		   exit ${ERRGENERIC}
 		}
 	fi
@@ -399,7 +448,6 @@ ls "${serverdir}"/ORB/*.orb | alt_ambig.pl --geosar="${serverdir}/DAT/GEOSAR/${o
 
 #create multilook
 
-ciop-log "INFO" "find ${serverdir}/DAT/GEOSAR/ -iname *.geosar -print  | ml_all.pl --type=byt --mlaz=10 --mlran=2 --dir=${serverdir}/SLC_CI2/ > ${serverdir}/log/ml.log 2<&1"
 
 find "${serverdir}/DAT/GEOSAR/" -iname "*.geosar"  -print | ml_all.pl --type=byt --mlaz=10 --mlran=2 --dir="${serverdir}/SLC_CI2/" > "${serverdir}/log/ml.log" 2<&1
 
@@ -410,23 +458,23 @@ precise_sm.pl --sm="${serverdir}/DAT/GEOSAR/${orbitmaster}.geosar" --serverdir="
 #precise_sm.pl --sm="${serverdir}/DAT/GEOSAR/${orbitslave}.geosar" --serverdir="${serverdir}" --recor --demdesc="${DEM}" --exedir="${EXE_DIR}" > "${serverdir}/log/precise_${orbitslave}.log" 2<&1
 
 #coregistration
-ciop-log "INFO : Running Image Coregistration"
+ciop-log "INFO"  "Running Image Coregistration"
 coreg.pl --master="${serverdir}/DAT/GEOSAR/${orbitmaster}.geosar"  --slave="${serverdir}/DAT/GEOSAR/${orbitslave}.geosar" --griddir="${serverdir}/GRID" --outdir="${serverdir}/GEO_CI2" --mltype=byt  --demdesc="${DEM}" --exedir="${EXE_DIR}" > "${serverdir}"/log/coregistration.log 2<&1
 
 
 #interferogram generation
 
 #ML Interf
-ciop-log "INFO : Running ML  Interferogram Generation"
+ciop-log "INFO"  "Running ML  Interferogram Generation"
 interf_sar.pl --master="${serverdir}/DAT/GEOSAR/${orbitmaster}.geosar" --slave="${serverdir}/DAT/GEOSAR/${orbitslave}.geosar" --ci2slave="${serverdir}"/GEO_CI2/geo_"${orbitslave}"_"${orbitmaster}".rad  --demdesc="${DEM}" --outdir="${serverdir}/DIF_INT" --exedir="${EXE_DIR}" --mlaz="${MLAZ}" --mlran="${MLRAN}" --amp --coh --nobort --noran --noinc --ortho --psfilt --orthodir="${serverdir}/GEOCODE"   > "${serverdir}/log/interf.log" 2<&1
  
 #11 Interf
-ciop-log "INFO : Running Full resolution Interferogram Generation"
+ciop-log "INFO"  "Running Full resolution Interferogram Generation"
 #interf_sar.pl --master="${serverdir}/DAT/GEOSAR/${orbitmaster}.geosar" --slave="${serverdir}/DAT/GEOSAR/${orbitslave}.geosar" --ci2slave="${serverdir}"/GEO_CI2/geo_"${orbitslave}"_"${orbitmaster}".rad --outdir="${serverdir}/DIF_INT" --exedir="${EXE_DIR}" --mlaz=1 --mlran=1 --amp --nocoh --nobort --noran --noinc > "${serverdir}/log/interf.log" 2<&1
  
  
 #ortho
-ciop-log "INFO : Running InSAR results ortho-projection"
+ciop-log "INFO"  "Running InSAR results ortho-projection"
 #ortho.pl --geosar="${serverdir}/DAT/GEOSAR/${orbitmaster}.geosar" --odir="${serverdir}/GEOCODE" --exedir="${EXE_DIR}" --tag="${orbitmaster}_${orbitslave}" --cplx --amp="${serverdir}/DIF_INT/amp_${orbitmaster}_${orbitslave}_ml11.r4" --pha="${serverdir}/DIF_INT/pha_${orbitmaster}_${orbitslave}_ml11.pha"  > "${serverdir}"/log/ortho.log 2<&1
  
  #ortho ML
@@ -445,7 +493,7 @@ ortho2geotiff.pl --ortho="${serverdir}/GEOCODE/amp_${orbitmaster}_${orbitslave}_
 
 
 #publish results
-ciop-log "INFO : Processing Ended. Publishing results"
+ciop-log "INFO"  "Processing Ended. Publishing results"
 #generated ortho files
 ciop-publish -m "${serverdir}"/GEOCODE/*.tif
 
